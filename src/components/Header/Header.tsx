23baa7a2d11753cdf8ce41d28ac2f2f427dfc66f
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-24 02:51:37 +0200

    MainLayout: Added video as background

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Search from "./Search/Search";
 
 const Header = () => {
   return (
-    <section className=" h-[55vh] bg-blueGray flex flex-col md:h-[65vh]">
+    <section className=" h-[55vh] bg-blueGray flex flex-col opacity-[0.99] md:h-[65vh]">
       <Navbar />
       <h1 className="text-5xl text-center my-6 mt-8 text-yellowGreen sm:text-6xl md:text-8xl">
         BookScape
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,8 +4,15 @@ import Header from "../components/Header/Header";
 const MainLayout = () => {
   return (
     <section>
+      <video
+        src="l2.mp4"
+        autoPlay
+        muted
+        loop
+        className="w-full fixed -z-10 opacity-25"
+      ></video>
       <Header />
-      <section className="border-2 border-blue-500 flex flex-col items-center min-h-screen md:w-[90%] lg:max-w-[1350px]  mx-auto ">
+      <section className="border-2 border-blue-500 flex flex-col items-center min-h-screen md:w-[90%] lg:max-w-[1350px]  mx-auto bg-backgroundColour opacity-[0.99]">
         <Outlet />
       </section>
     </section>
