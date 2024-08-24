00b32eda10460e9c7ca511b4cc20238a29e4785d
ThembaMtshelwane <thembamm3@gmail.com>
2024-08-24 23:59:28 +0200

    fixed

--- src/pages/SingleBook.tsx
+++ src/pages/SingleBook.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Book } from "../definitions";
 import SubHeader from "../components/SubHeader";
 import { useParams } from "react-router-dom";
